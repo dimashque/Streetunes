7c4924059d9d7845ffa8fb8fbbc5f3827d80805c
dimashque <dimashk89@hotmail.com>
2025-03-15 10:28:33 +0100

    Added EventApi

---
+++ Streetunes/Api/EventApiController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using Streetunes.Models;
+
+namespace Streetunes.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventApiController : ControllerBase
+    {
+        private readonly IEventReposiroty _eventRepository;
+
+        public EventApiController(IEventReposiroty eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Event>>> GetEvents()
+        {
+            var events = await _eventRepository.GetAll();
+            return Ok(events);
+                
+
+        }
+
+        [HttpGet("{id}")]
+        public  async Task <ActionResult<Event>> GetEvent(int id)
+        {
+            var streetevent =  await _eventRepository.GetByIdAsync(id);
+            return Ok(streetevent);
+        }
+    }
+}
